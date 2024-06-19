91d3888d83d734b636dc5aaa575c0696a264b524
Dushyant Supare <dushyant.supare@fau.de>
2024-06-19 19:36:02 +0200

    changing global variables to local variable

--- project/pipeline.py
+++ project/pipeline.py
@@ -3,23 +3,13 @@ import pandas as pd
 import sqlite3
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-# Call the Kaggle API through the existing library
-kaggleApiInit = KaggleApi()
-kaggleApiInit.authenticate()
-# Mention the kaggle dataset
-#Another dataset doesnt have a working URl so not calling it
-DATASET_FROM_KAGGLE = 'berkeleyearth/climate-change-earth-surface-temperature-data'  
-
-# As the dataset is a zip file and we need only one file from that
-WORKING_CSV_FILE = 'GlobalLandTemperaturesByCountry.csv'
-
 # A directory needs to be created for data, that is csv and sqllite file
 DATA_DIRECTORY = os.path.expanduser('./data')
 if not os.path.exists(DATA_DIRECTORY):
     os.makedirs(DATA_DIRECTORY)
 PROJECT_DIRECTORY = os.path.expanduser('./project')
 # Dataset Fetching would be done by this function from kaggle 
-def get_dataset(dataset_url):
+def get_dataset(dataset_url:str, kaggleApiInit : KaggleApi) -> str:
     local_directory_name = os.path.join(DATA_DIRECTORY, dataset_url.split('/')[-1]) # This will make a directory by the name of the dataset in local environment
     kaggleApiInit.dataset_download_files(dataset_url, path=local_directory_name, unzip=True)
     return local_directory_name
@@ -66,7 +56,14 @@ def create_sqlite_from_dataframe(dataframe, db_filename):
     conn.close()
 
 def main():
-    data_directory = get_dataset(DATASET_FROM_KAGGLE)
+    #Another dataset doesnt have a working URl so not calling it
+    DATASET_FROM_KAGGLE = 'berkeleyearth/climate-change-earth-surface-temperature-data'  
+
+    # As the dataset is a zip file and we need only one file from that
+    WORKING_CSV_FILE = 'GlobalLandTemperaturesByCountry.csv'
+    kaggleApiInit = KaggleApi()
+    kaggleApiInit.authenticate()
+    data_directory = get_dataset(DATASET_FROM_KAGGLE,kaggleApiInit)
     working_csv_file = os.path.join(data_directory, WORKING_CSV_FILE)
     dataset_directory_direct_file = './internally-displaced-persons-from-disasters.csv'
     if os.path.exists(dataset_directory_direct_file):
